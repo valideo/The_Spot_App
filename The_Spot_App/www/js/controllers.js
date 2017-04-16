923182b4d9b396d6f1b3c3d5f8cc9f5961cc13a3
Frezy Mboumba <maranathapp@outlook.com>
2017-04-16 23:08:54 +0200

    Commit bizarre

--- The_Spot_App/www/js/controllers.js
+++ The_Spot_App/www/js/controllers.js
@@ -1,6 +1,6 @@
 var module = angular.module('starter.controllers', []);
 
-module.controller('HomeCtrl', function ($scope, $http, $ionicModal, $state) {
+module.controller('HomeCtrl', function ($scope, $http, $ionicModal, $state, announceService) {
     $ionicModal.fromTemplateUrl('templates/login.html', {
         scope: $scope,
         animation: 'slide-in-up'
@@ -16,7 +16,7 @@ module.controller('HomeCtrl', function ($scope, $http, $ionicModal, $state) {
         $scope.openModalLogin();
     });
 
-    if (localStorage.getItem('isLoggedIn') == false) {
+    if (!localStorage.getItem('isLoggedIn')) {
       $scope.openModalLogin = function () {
           $scope.modal.show();
       };
@@ -51,6 +51,13 @@ module.controller('HomeCtrl', function ($scope, $http, $ionicModal, $state) {
         // Execute action
     });
 
+$scope.selectAnnounce = function(announce) {
+  $scope.selectedAnnounce = announce;
+  announceService.addAnnounce(announce);
+  console.log($scope.selectedAnnounce);
+}
+
+
   $http({
     method: 'GET',
     url: 'https://the-spot-app.herokuapp.com/api/announces'
@@ -65,7 +72,29 @@ module.controller('HomeCtrl', function ($scope, $http, $ionicModal, $state) {
 
   });
 
+  $scope.logOut = function (){
+    localStorage.removeItem('id_token',res.data.token);
+    localStorage.removeItem('user',JSON.stringify(res.data.user));
+    localStorage.setItem('isLoggedIn',false);
+  }
 
+});
+
+module.service('announceService', function() {
+  var announceList = [];
+
+  var addAnnounce = function(newObj) {
+      announceList.push(newObj);
+  };
+
+  var getAnnounces = function(){
+      return announceList;
+  };
+
+  return {
+    addAnnounce: addAnnounce,
+    getAnnounces: getAnnounces
+  };
 
 });
 
@@ -124,11 +153,7 @@ $scope.login = function() {
 
 });
 
-$scope.logOut = function (){
-  localStorage.removeItem('id_token',res.data.token);
-  localStorage.removeItem('user',JSON.stringify(res.data.user));
-  localStorage.setItem('isLoggedIn',false);
-}
+
 
 module.controller('registerCtrl', function ($scope, $http, $ionicPopup, $timeout) {
 
@@ -179,3 +204,70 @@ module.controller('registerCtrl', function ($scope, $http, $ionicPopup, $timeout
   }
 
 });
+
+module.controller('detailAnnounceCtrl', function ($scope, $http, announceService ) {
+  $scope.announces = announceService.getAnnounces();
+  console.log($scope.announces[$scope.announces.length -1]);
+  $scope.announce = $scope.announces[$scope.announces.length -1];
+  getPublisherInfo($scope.announce.publisherId);
+
+
+  function getPublisherInfo(uid) {
+    $http({
+      method: 'POST',
+      url: 'https://the-spot-app.herokuapp.com/api/users/profile',
+      data: { publisherId: uid }
+
+    }).then( res => {
+      console.log(res.data);
+      $scope.publisherInfo = res.data;
+    });
+}
+
+
+
+
+});
+
+
+module.controller('newAnnounceCtrl', function ($scope, $http ) {
+  $scope.newAnnounce = {};
+
+  $scope.categories = [{
+      value: 'Electromenager',
+      label: 'Electromenager'
+    }, {
+      value: 'Vetements',
+      label: 'Vetements'
+    },
+    {
+      value: 'Vehicules',
+      label: 'Vehicules'
+    },
+     {
+      value: 'High-tech',
+      label: 'High-tech'
+    },
+     {
+      value: 'Maison',
+      label: 'Maison'
+    }, {
+      value: 'Immobilier',
+      label: 'Immobilier'
+    }];
+
+    $scope.postAnnounce = function(){
+      // $scope.newAnnounce.publisherId = JSON.parse(localStorage.getItem('user')).uid;
+
+      $http({
+        method: 'POST',
+        url: 'https://the-spot-app.herokuapp.com/api/announces/add',
+        data: $scope.newAnnounce
+
+      }).then( res => {
+        console.log(res.data);
+      });
+      console.log($scope.newAnnounce);
+    }
+
+  });
